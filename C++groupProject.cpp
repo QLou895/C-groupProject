8bfb3403778499b556406bb07031ed0cc35460b8
LuisQuinones <quinlg@mail.broward.edu>
2020-12-02 15:02:55 -0500

    update: the menu validates that a number is entered.

--- C++groupProject.cpp
+++ C++groupProject.cpp
@@ -1,20 +1,51 @@
 // C++groupProject.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <iostream>
+// *********************** Prototypes ********************************* //
+int choice(int x);
 
+// *********************** functions ********************************** //
+
+
+#include <iostream>
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+    // variables 
+    int option;
+
+    // Ill be making this into a function 
+
+    printf("Welcome to the Everglades Ranger! \n Your timing is impeccable! We need you to go and find some Survivers!\n A group of people have sent an SoS and are stranded. You must hurry before one of the many dangerous \n animalas in the Everglades gets to them first.");
+    printf("Main Menu:\n");
+    printf("1) Play Game \n2) Game Rules \n3) Exit \n\n Enter choice: ");
+    cin >> option;
+    choice(option);
+
 }
+//try making this into a void function to see if the loop gives a better result
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+int choice(int x) {
+    int y;
+    do {
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+        switch (x) {
+        case 1:
+            cout << "You are plaing the game";
+            // here is where the start game function will go
+            break;
+        case 2:
+            cout << "These are the rules:";
+            printf("Main Menu:\n");
+            printf("1) Play Game \n2) Game Rules \n3) Exit \n\n Enter choice: ");
+            cin >> y;
+            choice(y);
+            break;
+        case 3:
+            return 0;
+        default:
+            printf("Error: code_Id_10t");
+            break;
+        }
+    } while (choice != 0);
+}
